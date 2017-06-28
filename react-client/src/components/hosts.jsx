cad396b38cbb93743d774ae1fd3ba3265f193824
Tiffany Yu <Tiffany@Tiffanys-MacBook-Pro-3.local>
2017-06-27 17:59:50 -0700

    added checklist, but does not save value

--- react-client/src/components/hosts.jsx
+++ react-client/src/components/hosts.jsx
@@ -6,7 +6,11 @@ import CreateEvent from './CreateEvent.jsx';
 
 =======
 import ReactDOM from 'react-dom';
+<<<<<<< HEAD
 >>>>>>> added tabs on vendors
+=======
+import CreateEvent from './CreateEvent.jsx';
+>>>>>>> added checklist, but does not save value
 
 class Hosts extends React.Component {
   constructor(props) {
@@ -48,14 +52,34 @@ class Hosts extends React.Component {
     }
   }
   
+	showTab(tab) {
+		this.setState({
+			showTab: tab
+		});
+	}
+	
+
   render() {
     return (
-			<div>
-				<h3>hi</h3>
+			<div> 
+				<div className="tab">
+					<button className="tablinks" onClick={()=>this.showTab('create')}>Create Event</button>
+					<button className="tablinks" onClick={()=>this.showTab('current')}>Current Events</button>
+					<button className="tablinks" onClick={()=>this.showTab('past')}>Past Events</button>
+				</div>
+				<div>
+					{this.state.showTab === 'create' ? <CreateEvent /> : null}
+					{this.state.showTab === 'current' ? <div>bidded list</div> : null}
+					{this.state.showTab === 'past' ? <div> matched list</div> : null}
+				</div>
 			</div>
-    );
+    )
   }
 }
 
+<<<<<<< HEAD
 >>>>>>> added tabs on vendors
+=======
+
+>>>>>>> added checklist, but does not save value
 export default Hosts;
