413fd4da3360dd8e3ce54f8b70889e72eab27253
Ryan Platon <ryanplaton@gmail.com>
2017-06-28 14:04:27 -0700

    Vendor component (#21)
    * Add utils.js for all client side ajax requests
    
    * add gitignore for DS_STORE files. Add mvp for vendors component

--- react-client/src/components/vendors.jsx
+++ react-client/src/components/vendors.jsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import utils from '../utils.js';
 import ReactDOM from 'react-dom';
 
 class Vendors extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      showTab: 'upcoming',
+      events: []
     }
+  }
+
+  componentDidMount() {
+
+    utils.getEvents(data => {
+
+      this.setState({
+        events: data
+      });
+
+    });
   }
 
 	showTab(tab) {
@@ -13,18 +27,39 @@ class Vendors extends React.Component {
 			showTab: tab
 		});
 	}
-	
+
 
   render() {
     return (
-			<div> 
+			<div>
 				<div className="tab">
 					<button className="tablinks" onClick={()=>this.showTab('upcoming')}>Upcoming Events</button>
 					<button className="tablinks" onClick={()=>this.showTab('bidded')}>Bidded Events</button>
 					<button className="tablinks" onClick={()=>this.showTab('matched')}>Matched Events</button>
 				</div>
 				<div>
-					{this.state.showTab === 'upcoming' ? <div>upcoming list</div> : null}
+					{this.state.showTab === 'upcoming'
+          ? <div>
+              <div>{this.state.events.map(event =>
+                      <div>
+                        <div>{event.date}</div>
+                        <div>{event.location}</div>
+                        <div>Needs: {Object.keys(event.needs).map(need =>
+                                <div>
+                                  <div>{need}</div>
+                                  <div>
+                                    {Object.keys(event.needs[need]).map(item =>
+                                      <li>{item === 'budget' ? 'Budget: ' + event.needs[need].budget: item}</li>
+                                    )}
+                                  </div>
+                                </div>
+                             )}
+                        </div>
+                      </div>
+                   )}
+              </div>
+            </div>
+          : null}
 					{this.state.showTab === 'bidded' ? <div>bidded list</div> : null}
 					{this.state.showTab === 'matched' ? <div> matched list</div> : null}
 				</div>
@@ -36,3 +71,5 @@ class Vendors extends React.Component {
 }
 
 export default Vendors;
+
+//{_id, userid, needs, location, date, __v, vendors}
