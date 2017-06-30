e0b98001efec7b84f5aa4861d26b538ec278f539
Peter X. Tan <pxhtan@gmail.com>
2017-06-30 14:34:30 -0700

    Add a toggle feature for the needs of the host (#37)

--- react-client/src/components/CurrentEvents.jsx
+++ react-client/src/components/CurrentEvents.jsx
@@ -1,43 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ShowNeedsHost from './ShowNeedsHost.jsx';
+import $ from 'jquery';
 
 class CurrentEvents extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+			isHidden: true,
+			showEvent: 0
 		}
 	}
+
+
 
 	render() {
 		return (
 			<div>
-				<div>{this.props.events.map(event =>
-					<div className="card">
-						<div className="card-block">
-							<h4 className="card-title font-weight-bold text-primary">{event.title}</h4>
-							<h6 className="card-subtitle mb-2 text-muted">Date: {event.date}</h6>
-							<h6 className="card-subtitle mb-2 text-muted">Location: {event.location}</h6>
-							<div>
-								<h5 className="font-weight-bold-italic">Needs: </h5> {
-									Object.keys(event.needs).map(need => 
-										<div className="container">
-											<div className="row">{need}</div>
-											<div className="col-xl-2">
-												{
-													Object.keys(event.needs[need]).map( item => 
-														<li>{item === 'budget' ? 'Budget: ' + event.needs[need].budget: item}</li>
-													)
-												}
-											</div>
-										</div>
-									)
-								}
-							</div>
-
-						</div>					
-					</div>
-				)}
-				</div>
+				{
+				this.props.events.map((event, idx) =>
+					<ShowNeedsHost event={event} idx={idx}/>
+				)
+			}
 			</div>
 		);
 	}
@@ -45,3 +29,6 @@ class CurrentEvents extends React.Component {
 
 export default CurrentEvents
 
+/*
+
+ */
